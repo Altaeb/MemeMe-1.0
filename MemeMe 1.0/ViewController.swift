1a43793ea6058930b39c452f667414b21ef146a2
aftiyah <abd2010ly@gmail.com>
2020-04-05 15:25:58 +0200

    Launching the image picker

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pickerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        self.present(pickerController, animated: true, completion: nil)
+    }
+    
 
 }
 
